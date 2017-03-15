fc14671bedeb44a82c5efa748b789166cd1f8b18
dmoser <david.moser@bitmovin.com>
2017-03-15 13:44:46 +0100

    Implemented list and specific functions for vod and live statistics

--- bitmovin/encoding/statistics.js
+++ bitmovin/encoding/statistics.js
@@ -1,8 +1,39 @@
 import urljoin from 'url-join';
-import http from '../http';
+import {utils} from '../http';
 
 export const statistics = (configuration, http) => {
   const { get } = http;
+
+  const fn = (url) => {
+    return {
+      list: (limit, offset) => {
+        const getParams = utils.buildGetParamString({
+          limit : limit,
+          offset: offset
+        });
+        if (getParams.length > 0) {
+          url = urljoin(url, getParams);
+        }
+
+        return get(configuration, url)
+      },
+      specific: (startDate, endDate, limit, offset) => {
+
+        let url = urljoin(url, startDate, endDate);
+
+        const getParams = utils.buildGetParamString({
+          limit : limit,
+          offset: offset
+        });
+        if (getParams.length > 0) {
+          url = urljoin(url, getParams);
+        }
+
+        return get(configuration, url)
+      }
+    }
+  };
+
   return {
     overall : () => {
       const url = urljoin(configuration.apiBaseUrl, 'encoding/statistics');
@@ -13,6 +44,14 @@ export const statistics = (configuration, http) => {
         liveStatistics: () => {
           const url = urljoin(configuration.apiBaseUrl, 'encoding/statistics/encodings', encodingId, 'live-statistics');
           return get(configuration, url);
+        },
+        vod: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/statistics/encodings/vod');
+          return fn(url);
+        },
+        live: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/statistics/encodings/live');
+          return fn(url);
         }
       }
     }
