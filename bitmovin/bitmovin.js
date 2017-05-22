9404899815fc3f7872eae75506b4c8e3cffdaa4d
dmoser <david.moser@bitmovin.com>
2017-05-22 13:01:25 +0200

    Changed version to 1.0.13

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -61,7 +61,7 @@ export default class Bitmovin {
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
       'X-Api-Client'        : 'bitmovin-javascript',
-      'X-Api-Client-Version': '1.0.12'
+      'X-Api-Client-Version': '1.0.13'
     };
 
     this.configuration = configuration;
