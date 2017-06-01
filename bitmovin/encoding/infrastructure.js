c2d3355e9979f95d999e02379813fea04a1157e8
dmoser <david.moser@bitmovin.com>
2017-06-01 15:13:11 +0200

    Adjusted infrastructure typeFn. Added tests

--- bitmovin/encoding/infrastructure.js
+++ bitmovin/encoding/infrastructure.js
@@ -5,33 +5,40 @@ export const infrastructure = (configuration, http) => {
   const { get, post, delete_ } = http;
 
   const typeFn = (type) => {
-    return {
-      list: () => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type);
-        return get(configuration, url);
-      },
-      status: (id) => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id, 'status');
-        return get(configuration, url);
+
+    let fn = (id) => {
+      return {
+        status: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id, 'status');
+          return get(configuration, url);
+        }
+        ,
+        details: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id);
+          return get(configuration, url);
+        },
+        delete: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id);
+          return delete_(configuration, url);
+        },
+        customData: () => {
+          const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id, 'customData');
+          return get(configuration, url);
+        }
       }
-      ,
-      details: (id) => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id);
-        return get(configuration, url);
-      },
-      delete: (id) => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id);
-        return delete_(configuration, url);
-      },
-      customData: (id) => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type, id, 'customData');
-        return get(configuration, url);
-      },
-      create: (infrastructure) => {
-        const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type);
-        return post(configuration, url, infrastructure);
-      }
-    }
+    };
+
+    fn.create =  (infrastructure) => {
+      const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type);
+      return post(configuration, url, infrastructure);
+    };
+
+    fn.list = () => {
+      const url = urljoin(configuration.apiBaseUrl, 'encoding/infrastructure', type);
+      return get(configuration, url);
+    };
+
+    return fn;
   };
 
   return {
