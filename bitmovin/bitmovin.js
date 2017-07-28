ca97f7e2a7490ed6046b21ac30e8f64d870bf99b
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-07-28 11:36:30 -0700

    made x api client header configurable

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -56,13 +56,17 @@ export default class Bitmovin {
       configuration.requestTimeout = 30000;
     }
 
+    if (configuration.xApiClient === undefined) {
+      configuration.xApiClient = 'bitmovin-javascript';
+    }
+
     configuration.apiBaseUrl = urljoin(configuration.protocol + '://' + configuration.host, configuration.basePath);
 
     configuration.httpHeaders = {
       'Content-Type'        : 'application/json',
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
-      'X-Api-Client'        : 'bitmovin-javascript',
+      'X-Api-Client'        : configuration.xApiClient,
       'X-Api-Client-Version': '1.1.8'
     };
 
