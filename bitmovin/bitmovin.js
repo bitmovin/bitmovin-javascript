d4490e5134efcdc2e2a5b9dbeeadb781f00a76bc
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-07-28 15:04:05 -0700

    bumped to version 1.1.11

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -67,7 +67,7 @@ export default class Bitmovin {
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
       'X-Api-Client'        : configuration.xApiClient,
-      'X-Api-Client-Version': '1.1.10'
+      'X-Api-Client-Version': '1.1.11'
     };
 
     this.configuration = configuration;
