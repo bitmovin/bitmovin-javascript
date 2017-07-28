28e575069eff5b4cebf04811fe5c6cccd12103d2
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-07-28 15:00:51 -0700

    fixed merge issue

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -66,7 +66,7 @@ export default class Bitmovin {
       'Content-Type'        : 'application/json',
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
-      'X-Api-Client'        : 'bitmovin-javascript',
+      'X-Api-Client'        : configuration.xApiClient,
       'X-Api-Client-Version': '1.1.10'
     };
 
