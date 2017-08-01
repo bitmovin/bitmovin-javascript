1040c029a2140f3111fe93d482e911107405b5cf
dmoser <david.moser@bitmovin.com>
2017-08-01 15:34:42 +0200

    Added http tests

--- tests/utils/http.test.js
+++ tests/utils/http.test.js
@@ -2,7 +2,6 @@ import {get} from '../../bitmovin/http'
 
 describe('Test HTTP Client', () => {
   it('should resolve the promise if the response status is 204', () => {
-
     const constMockConfiguration = {
       httpHeaders: ['X-Api-Key: someApiKey'],
       requestTimeOut: 5000
@@ -19,6 +18,44 @@ describe('Test HTTP Client', () => {
       expect(true).toEqual(true)
     }).catch((error) => {
       expect(false).toEqual(true)
+    });
+  });
+
+  it('should reject the promise if the response status is > 399', () => {
+
+    const constMockConfiguration = {
+      httpHeaders: ['X-Api-Key: someApiKey'],
+      requestTimeOut: 5000
+    };
+
+    const errorData = {
+      requestId: '23e71f50-aca9-49be-8bb2-d50ee6b5cd35',
+      status: 'ERROR',
+      data: {
+        code: 1001,
+        message: 'An encoding with the given id does not exist',
+        developerMessage: 'Id not found, check the encoding list for valid ids'
+      }
+    };
+
+    const mockResponse = {
+      status: 404,
+      statusText: 'NOT FOUND',
+      json: () => {
+        return Promise.resolve(errorData);
+      }
+    };
+
+    const mockFetch = jest.fn(() => {
+      return Promise.resolve(mockResponse)
+    });
+
+    return get(constMockConfiguration, "http://someurl.com/some/resources", mockFetch).then(() => {
+      expect(false).toEqual(true)
+    }).catch((bitmovinError) => {
+      expect(bitmovinError.response.status).toEqual(mockResponse.status);
+      expect(bitmovinError.response.statusText).toEqual(mockResponse.statusText)
+      expect(bitmovinError.response.responseData).toEqual(errorData);
     });
   });
 });
