728346e8e4107578e783ed602280f15046c24007
dmoser <david.moser@bitmovin.com>
2017-08-03 13:16:20 +0200

    Added to test post and delete method

--- tests/utils/http.test.js
+++ tests/utils/http.test.js
@@ -1,4 +1,4 @@
-import {get} from '../../bitmovin/http'
+import {delete_, get, post} from '../../bitmovin/http'
 
 describe('Test HTTP Client', () => {
   it('should resolve the promise if the response status is 204', () => {
@@ -22,7 +22,6 @@ describe('Test HTTP Client', () => {
   });
 
   it('should reject the promise if the response status is > 399', () => {
-
     const constMockConfiguration = {
       httpHeaders: ['X-Api-Key: someApiKey'],
       requestTimeOut: 5000
@@ -54,7 +53,7 @@ describe('Test HTTP Client', () => {
       expect(false).toEqual(true)
     }).catch((bitmovinError) => {
       expect(bitmovinError.response.status).toEqual(mockResponse.status);
-      expect(bitmovinError.response.statusText).toEqual(mockResponse.statusText)
+      expect(bitmovinError.response.statusText).toEqual(mockResponse.statusText);
       expect(bitmovinError.response.responseData).toEqual(errorData);
     });
   });
@@ -96,5 +95,160 @@ describe('Test HTTP Client', () => {
     }).catch((error) => {
       expect(false).toEqual(true)
     });
+  });
+
+  it('should do a successfull post', () => {
+    const constMockConfiguration = {
+      httpHeaders: ['X-Api-Key: someApiKey'],
+      requestTimeOut: 5000
+    };
+
+    const successData = {
+      requestId: '33fc02af-5b81-4948-81ef-71c999c017f2',
+      status: 'SUCCESS',
+      data: {
+        result: {
+          name: 'Production-ID-678',
+          description: 'Project ID: 567',
+          id: 'cb90b80c-8867-4e3b-8479-174aa2843f62',
+          createdAt: '2016-06-25T20:09:23.69Z',
+          modifiedAt: '2016-06-25T20:09:23.69Z'
+        }
+      }
+    };
+
+    const mockResponse = {
+      status: 200,
+      statusText: 'OK',
+      json: () => {
+        return Promise.resolve(successData);
+      }
+    };
+
+    const mockFetch = jest.fn(() => {
+      return Promise.resolve(mockResponse)
+    });
+
+    const someObject = {
+      foo: 'bar'
+    };
+
+    return post(constMockConfiguration, "http://someurl.com/some/resources", someObject, mockFetch).then((response) => {
+      expect(response).toEqual(successData.data.result);
+    }).catch((error) => {
+      expect(false).toEqual(true)
+    });
+  });
+
+  it('should fail a post request with error code 400', () => {
+    const constMockConfiguration = {
+      httpHeaders: ['X-Api-Key: someApiKey'],
+      requestTimeOut: 5000
+    };
+
+    const errorData = {
+      requestId: '33fc02af-5b81-4948-81ef-71c999c017f2',
+      status: 'ERROR',
+      data: {
+        code: 1001,
+        message: 'Wrong parameters'
+      }
+    };
+
+    const mockResponse = {
+      status: 400,
+      statusText: 'BAD_REQUEST',
+      json: () => {
+        return Promise.resolve(errorData);
+      }
+    };
+
+    const mockFetch = jest.fn(() => {
+      return Promise.resolve(mockResponse)
+    });
+
+    const someObject = {
+      foo: 'bar'
+    };
+
+    return post(constMockConfiguration, "http://someurl.com/some/resources", someObject, mockFetch).then((response) => {
+      expect(false).toEqual(true)
+    }).catch((bitmovinError) => {
+      expect(bitmovinError.response.status).toEqual(mockResponse.status);
+      expect(bitmovinError.response.statusText).toEqual(mockResponse.statusText);
+      expect(bitmovinError.response.responseData).toEqual(errorData);
+    });
+  });
+
+  it('should do a successfull delete', () => {
+    const constMockConfiguration = {
+      httpHeaders: ['X-Api-Key: someApiKey'],
+      requestTimeOut: 5000
+    };
+
+    const successData = {
+      requestId: '33fc02af-5b81-4948-81ef-71c999c017f2',
+      status: 'SUCCESS',
+      data: {
+        result: {
+          id: 'cb90b80c-8867-4e3b-8479-174aa2843f62',
+        }
+      }
+    };
+
+    const mockResponse = {
+      status: 200,
+      statusText: 'OK',
+      json: () => {
+        return Promise.resolve(successData);
+      }
+    };
+
+    const mockFetch = jest.fn(() => {
+      return Promise.resolve(mockResponse)
+    });
+
+    return delete_(constMockConfiguration, "http://someurl.com/some/resources", mockFetch).then((response) => {
+      expect(response).toEqual(successData.data.result);
+    }).catch((error) => {
+      expect(false).toEqual(true)
+    });
+  });
+
+  it('should fail a delete request with error code 400', () => {
+    const constMockConfiguration = {
+      httpHeaders: ['X-Api-Key: someApiKey'],
+      requestTimeOut: 5000
+    };
+
+    const errorData = {
+      requestId: '33fc02af-5b81-4948-81ef-71c999c017f2',
+      status: 'ERROR',
+      data: {
+        code: 1001,
+        message: 'Id not found'
+      }
+    };
+
+    const mockResponse = {
+      status: 404,
+      statusText: 'NOT_FOUND',
+      json: () => {
+        return Promise.resolve(errorData);
+      }
+    };
+
+    const mockFetch = jest.fn(() => {
+      return Promise.resolve(mockResponse)
+    });
+
+    return delete_(constMockConfiguration, "http://someurl.com/some/resources", mockFetch).then((response) => {
+      expect(false).toEqual(true)
+    }).catch((bitmovinError) => {
+      expect(bitmovinError.response.status).toEqual(mockResponse.status);
+      expect(bitmovinError.response.statusText).toEqual(mockResponse.statusText);
+      expect(bitmovinError.response.responseData).toEqual(errorData);
+    });
   })
+
 });
