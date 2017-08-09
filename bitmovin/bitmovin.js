4d9b046cd7672ae8505f302a90798a5fe8a78060
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-08-09 15:23:40 -0700

    level up

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -67,7 +67,7 @@ export default class Bitmovin {
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
       'X-Api-Client'        : configuration.xApiClient,
-      'X-Api-Client-Version': '1.1.13'
+      'X-Api-Client-Version': '1.1.14'
     };
 
     this.configuration = configuration;
