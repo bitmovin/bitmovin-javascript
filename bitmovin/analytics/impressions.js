7bbb5745efc21ad3c7b1039511b8fccb6fff064f
Daniel Hoelbling-Inzko <daniel@hoelbling-inzko.com>
2017-09-08 14:18:28 +0200

    Added correct handling of licenseKeys to impressions call

--- bitmovin/analytics/impressions.js
+++ bitmovin/analytics/impressions.js
@@ -2,16 +2,12 @@ import urljoin from 'url-join';
 import http, { utils } from '../http';
 
 export const impressions = (configuration, http) => {
-  const { get } = http;
+  const { post } = http;
   const impressionBaseUrl = urljoin(configuration.apiBaseUrl, 'analytics', 'impressions');
 
-  const fn = (impressionId) => {
-    return {
-      details: () => {
-        let url = urljoin(impressionBaseUrl, impressionId);
-        return get(configuration, url);
-      }
-    }
+  const fn = (impressionId, licenseKey) => {
+    let url = urljoin(impressionBaseUrl, impressionId);
+    return post(configuration, url, { licenseKey });
   };
 
   return fn;
