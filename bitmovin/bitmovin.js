6445cb5254b6666f1c736b67d1c01d2a67718abb
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-09-22 09:22:51 +0200

    fixed version for client header

--- bitmovin/bitmovin.js
+++ bitmovin/bitmovin.js
@@ -69,7 +69,7 @@ export default class Bitmovin {
       'X-Api-Key'           : configuration.apiKey,
       'X-Tenant-Org-Id'     : configuration.tenantOrgId,
       'X-Api-Client'        : configuration.xApiClient,
-      'X-Api-Client-Version': '1.1.17'
+      'X-Api-Client-Version': '1.1.19'
     };
 
     this.configuration = configuration;
