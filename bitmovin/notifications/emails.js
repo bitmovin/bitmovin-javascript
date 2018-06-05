47b05dac08287c164b0894a7dab18923d2323e55
domysee <me@dominik-weber.name>
2018-06-05 11:44:07 +0200

    Wrap email notifications

---
+++ bitmovin/notifications/emails.js
@@ -0,0 +1,38 @@
+// @flow
+import urljoin from 'url-join';
+
+import http, {utils} from '../utils/http';
+import type {BitmovinConfiguration, HttpClient, List} from '../utils/types';
+
+import type {EmailNotificationWithConditionsDetails} from './types';
+
+const emails = (configuration: BitmovinConfiguration, http: HttpClient = http): NotificationEmails => {
+  const notificationsBaseUrl = urljoin(configuration.apiBaseUrl, 'notifications');
+
+  const list = (limit, offset, sort, filter) => {
+    let url = urljoin(notificationsBaseUrl, 'emails', 'encoding', 'encodings', 'live-input-stream-changed');
+
+    const filterParams = utils.buildFilterParamString(filter);
+    let getParams = utils.buildGetParamString({
+      ...filterParams,
+      limit: limit,
+      offset: offset,
+      sort: sort
+    });
+    if (getParams.length > 0) {
+      url = urljoin(url, getParams);
+    }
+
+    return http.get(configuration, url);
+  };
+
+  return {
+    list
+  };
+};
+
+export type NotificationEmails = {
+  list: List<EmailNotificationWithConditionsDetails>
+};
+
+export default emails;
--- bitmovin/notifications/notifications.js
+++ bitmovin/notifications/notifications.js
@@ -1,38 +1,16 @@
 // @flow
-import urljoin from 'url-join';
+import type {BitmovinConfiguration, HttpClient} from '../utils/types';
 
-import http, {utils} from '../utils/http';
-import type {BitmovinConfiguration, HttpClient, List} from '../utils/types';
-
-import type {EmailNotificationWithConditionsDetails} from './types';
-
-const notifications = (configuration: BitmovinConfiguration, http: HttpClient = http): Notifications => {
-  const notificationsBaseUrl = urljoin(configuration.apiBaseUrl, 'notifications');
-
-  const list = (limit, offset, sort, filter) => {
-    let url = urljoin(notificationsBaseUrl, 'emails', 'encoding', 'encodings', 'live-input-stream-changed');
-
-    const filterParams = utils.buildFilterParamString(filter);
-    let getParams = utils.buildGetParamString({
-      ...filterParams,
-      limit: limit,
-      offset: offset,
-      sort: sort
-    });
-    if (getParams.length > 0) {
-      url = urljoin(url, getParams);
-    }
-
-    return http.get(configuration, url);
-  };
-
+import emails from './emails'
+import type {NotificationEmails} from './emails'
+ 
+export const notifications = (configuration: BitmovinConfiguration, http: HttpClient): Notifications => {
   return {
-    list
-  };
+    emails: emails(configuration, http)
+  }
 };
 
 export type Notifications = {
-  list: List<EmailNotificationWithConditionsDetails>
+  emails: NotificationEmails
 };
 
-export default notifications;
