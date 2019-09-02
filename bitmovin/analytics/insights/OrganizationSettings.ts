a35049594ad32becb4575cb514668ad0e03c2158
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-02 16:33:00 +0200

    Added class for organization settings

---
+++ bitmovin/analytics/insights/OrganizationSettings.ts
@@ -0,0 +1,30 @@
+import * as urljoin from 'url-join';
+
+import http from '../../utils/http';
+import {HttpClient, InternalConfiguration} from '../../utils/types';
+
+export interface Settings {
+  orgId: string;
+  includeInInsights: boolean;
+  industry: string;
+  subIndustry: string;
+  isTrial: boolean;
+}
+
+export default class OrganizationSettings {
+  private baseUrl: string;
+
+  constructor(private configuration: InternalConfiguration, urlPath: string, private httpClient: HttpClient = http) {
+    this.configuration = configuration;
+    this.baseUrl = urljoin(configuration.apiBaseUrl, urlPath, 'organizations');
+  }
+
+  public settings = (orgId: string) => ({
+    details: (): Promise<Settings> => {
+      return this.httpClient.get(this.configuration, urljoin(this.baseUrl, orgId, 'settings'));
+    },
+    update: (settings: {includeInInsights: boolean}): Promise<Settings> => {
+      return this.httpClient.put(this.configuration, urljoin(this.baseUrl, orgId, 'settings'), settings);
+    }
+  })
+}
